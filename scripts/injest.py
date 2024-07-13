a601b2786d514698d0d2dce1772d0d15cba31c35
Ryan <durosyan@outlook.com>
2024-07-13 13:08:09 +0100

    scripting

--- scripts/injest.py
+++ scripts/injest.py
@@ -1,2 +1,11 @@
-# will injest an existing directory into an empty sqlite database.
+'''
+will injest an existing directory into an empty sqlite database.
+finds all files in a directory with the correct text header as follows:
+---
+title: ""
+posted: "HHMM dd/mm/yyyy
+mood: xx%
+---
 
+content.
+'''
