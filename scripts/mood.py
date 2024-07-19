582d25bed396e8365df9bfffa3988f6ac84294ec
durosyan <durosyan@outlook.com>
2024-07-19 09:36:34 +0100

    check

--- scripts/mood.py
+++ scripts/mood.py
@@ -12,8 +12,6 @@ then parses said text header into a json blob file output
 '''
 
 import argparse
-import json
-import re
 import os
 import markdown
 import pandas as pd
@@ -23,19 +21,11 @@ from datetime import datetime
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="personal markdown journal")
     parser.add_argument("directory")
-    # parser.add_argument("index")
     args = parser.parse_args()
 
     directory = os.path.abspath(args.directory)
-    # index = os.path.abspath(args.index)
-
-    # entries = []
     entry_dates = []
     moods = []
-
-    # if os.path.exists(index):
-    #     with open(index, 'r') as index_file:
-    #         entries = json.load(index_file)
 
     for root, dirs, files in os.walk(directory):
         for name in files:
@@ -53,11 +43,6 @@ if __name__ == "__main__":
                         moods.append(mood)
                         entry_dates.append(date_obj)
                         print(os.path.basename(file.name))
-
-                        
-    # # Write the extracted data to the JSON index file
-    # with open(index, 'w') as index_file:
-    #     json.dump(entries, index_file, indent=4)
     date_time = pd.to_datetime(entry_dates)
     DF = pd.DataFrame()
     DF['value'] = moods
@@ -65,5 +50,3 @@ if __name__ == "__main__":
     plt.plot(DF)
     plt.gcf().autofmt_xdate()
     plt.show()
-
-    # print(f"Processed {len(entries)} files. Data saved to {index}")
