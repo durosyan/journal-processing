701a49ce0d2aede1dbdd6a80ba55b5872033b797
Ryan <155094064+durosyan@users.noreply.github.com>
2024-07-21 12:05:42 +0000

    basic go scli

--- scripts/new.go
+++ scripts/new.go
@@ -11,26 +11,12 @@ import (
 func main() {
 	// Define command line arguments
 	pwd, err := os.Getwd()
-	filePath := flag.String("template", "", "Path to the template file")
 	entryTitle := flag.String("title", "", "Entry title")
 	outputDir := flag.String("output", pwd, "Output directory")
 	verbose := flag.Bool("verbose", false, "Enable verbose mode")
 
 	// Parse command line arguments
 	flag.Parse()
-
-	if *filePath == "" {
-		fmt.Println("Error: -file argument is required")
-		flag.Usage()
-		os.Exit(1)
-	}
-
-	// Resolve the file path
-	absFilePath, err := filepath.Abs(*filePath)
-	if err != nil {
-		fmt.Println("Error resolving file path:", err)
-		os.Exit(1)
-	}
 
 	// Resolve the output directory
 	absOutputDir, err := filepath.Abs(*outputDir)
@@ -39,40 +25,34 @@ func main() {
 		os.Exit(1)
 	}
 
+	// define title
+	if *entryTitle == "" {
+		// ask the user for the title
+		fmt.Print("Enter the title of the entry: ")
+		fmt.Scanln(entryTitle)
+	}
+
+	// define mood
+	var mood string
+	fmt.Print("Enter the mood of the entry between 0 and 100: ")
+	fmt.Scanln(&mood)
+
 	// Access the parsed arguments
 	if *verbose {
 		fmt.Println("Verbose Mode:", *verbose)
 		fmt.Println("Entry Title:", *entryTitle)
-		fmt.Println("File Path:", absFilePath)
 	}
-
-	// Read the file
-	content, err := os.Open(absFilePath)
-	if err != nil {
-		fmt.Println("Error reading file:", err)
-		return
-	}
-
-	// Read the file content
-	fileContent := make([]byte, 1024)
-	_, err = content.Read(fileContent)
-	if err != nil {
-		fmt.Println("Error reading file:", err)
-		return
-	}
-
-	// Get current date and time
-	date := time.Now().Format("150402012006")
 
 	// Create the output file
-	outputFile, err := os.Create(filepath.Join(absOutputDir, "Entry_"+date+".md"))
+	outputFile, err := os.Create(filepath.Join(absOutputDir, "Entry_"+time.Now().Format("150402012006")+".md"))
 	if err != nil {
 		fmt.Println("Error creating output file:", err)
 		return
 	}
 
 	// Write the entry title to the output file
-	_, err = outputFile.WriteString(fmt.Sprintf("--- \n%s\n", *entryTitle))
+	// Write the entry title and date to the output file
+	_, err = outputFile.WriteString(fmt.Sprintf("--- \nTitle: %s\nDate: %s\nMood: %s\n---\n", *entryTitle, time.Now().Format("1504 02/01/2006"), mood))
 	if err != nil {
 		fmt.Println("Error writing title to output file:", err)
 		return
