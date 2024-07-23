c3e3dd0fbe12e4307d322a4fa8995be2a45613e6
durosyan <durosyan@outlook.com>
2024-07-23 22:28:45 +0100

    probably not best practice

--- go-lang/new.go
+++ go-lang/new.go
@@ -1,17 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 	"time"
 )
 
 func main() {
 	// Define command line arguments
 	pwd, err := os.Getwd()
-	entryTitle := flag.String("title", "", "Entry title")
 	outputDir := flag.String("output", pwd, "Output directory")
 	verbose := flag.Bool("verbose", false, "Enable verbose mode")
 
@@ -26,21 +27,23 @@ func main() {
 	}
 
 	// define title
-	if *entryTitle == "" {
-		// ask the user for the title
-		fmt.Print("Enter the title of the entry: ")
-		fmt.Scanln(entryTitle)
-	}
+	var entryTitle string
+	fmt.Print("Enter the title of the entry: ")
+	readerTitle := bufio.NewReader(os.Stdin)      // Create a new buffered reader
+	titleInput, _ := readerTitle.ReadString('\n') // Read the entire line
+	entryTitle = strings.TrimSpace(titleInput)    // Trim newline character
 
 	// define mood
 	var mood string
 	fmt.Print("Enter the mood of the entry between 0 and 100: ")
-	fmt.Scanln(&mood)
+	readerMood := bufio.NewReader(os.Stdin)     // Create a new buffered reader
+	moodInput, _ := readerMood.ReadString('\n') // Read the entire line
+	mood = strings.TrimSpace(moodInput)         // Trim newline character
 
 	// Access the parsed arguments
 	if *verbose {
 		fmt.Println("Verbose Mode:", *verbose)
-		fmt.Println("Entry Title:", *entryTitle)
+		fmt.Println("Entry Title:", entryTitle)
 	}
 
 	// Create the output file
@@ -52,7 +55,7 @@ func main() {
 
 	// Write the entry title to the output file
 	// Write the entry title and date to the output file
-	_, err = outputFile.WriteString(fmt.Sprintf("--- \nTitle: %s\nDate: %s\nMood: %s\n---\n", *entryTitle, time.Now().Format("1504 02/01/2006"), mood))
+	_, err = outputFile.WriteString(fmt.Sprintf("--- \nTitle: %s\nDate: %s\nMood: %s\n---\n", entryTitle, time.Now().Format("1504 02/01/2006"), mood))
 	if err != nil {
 		fmt.Println("Error writing title to output file:", err)
 		return
