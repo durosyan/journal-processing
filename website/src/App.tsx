9db5c5edf584d9e61007f763628c3aa8481b26fe
durosyan <durosyan@outlook.com>
2024-07-26 18:15:12 +0100

    removed commented

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -4,39 +4,21 @@ import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
-// import FormControlLabel from '@mui/material/FormControlLabel';
 import IconButton from '@mui/material/IconButton';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import MenuItem from '@mui/material/MenuItem';
-// import Modal from '@mui/material/Modal';
-// import Switch from '@mui/material/Switch';
 import TextField from '@mui/material/TextField';
 import Toolbar from '@mui/material/Toolbar';
 import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
-
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-
 import Slider from './Slider.tsx';
-// import './App.css';
 
 const pages = ['Products', 'Pricing', 'Blog'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
-
-// const modalStyle = {
-//     position: 'absolute' as 'absolute',
-//     top: '50%',
-//     left: '50%',
-//     transform: 'translate(-50%, -50%)',
-//     width: 400,
-//     bgcolor: 'background.paper',
-//     border: '2px solid #000',
-//     boxShadow: 24,
-//     p: 4,
-// };
 
 function App() {
     const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
@@ -58,10 +40,6 @@ function App() {
     };
 
     const [value, setValue] = React.useState<Date | null>(null);
-
-    // const [open, setOpen] = React.useState(false);
-    // const handleOpen = () => setOpen(true);
-    // const handleClose = () => setOpen(false);
 
     return (
         <Container maxWidth="xl" sx={{ p: '0px !important' }}>
@@ -181,9 +159,6 @@ function App() {
                     <div>
                         <TextField id="exampleTextarea" label="log" multiline rows={4} />
                     </div>
-                    {/* <div>
-                        <FormControlLabel control={<Switch defaultChecked />} label="Example switch" />
-                    </div> */}
                     <div>
                         <LocalizationProvider dateAdapter={AdapterDateFns}>
                             <DatePicker
@@ -199,25 +174,6 @@ function App() {
                     <div>
                         <Slider />
                     </div>
-                    {/* <div>
-                        <Button variant="contained" onClick={handleOpen}>Open modal</Button>
-                        <Modal
-                            open={open}
-                            onClose={handleClose}
-                            aria-labelledby="modal-modal-title"
-                            aria-describedby="modal-modal-description"
-                        >
-                            <Box sx={modalStyle}>
-                                <Typography id="modal-modal-title" variant="h6" component="h2">
-                                    Text in a modal
-                                </Typography>
-                                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                                    Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                                </Typography>
-                                <Button variant="contained" onClick={handleClose} sx={{ mt: 2 }}>Close modal</Button>
-                            </Box>
-                        </Modal>
-                    </div> */}
                 </Container>
             </Box>
         </Container>
