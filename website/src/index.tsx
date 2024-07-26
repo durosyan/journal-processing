81355789b2aebe2a28c50cfc3dd6af57415b39d4
durosyan <durosyan@outlook.com>
2024-07-26 09:45:10 +0100

    react 18

--- website/src/index.tsx
+++ website/src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 // import './index.css';
 import App from './App.tsx';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+const node = document.getElementById('root');
 
+if (node) {
+    createRoot(node).render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
+}
